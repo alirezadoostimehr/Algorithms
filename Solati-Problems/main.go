315e275a3d38806ec9062aacaeadb6537a19f20a
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-15 13:16:34 +0330

    new problem

--- Solati-Problems/main.go
+++ Solati-Problems/main.go
@@ -3,29 +3,17 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func run(a []int) ([]int, []int) {
-	cnt := make(map[int]int)
-
-	for _, val := range a {
-		cnt[val]++
-	}
-	un := make([]int, 0)
-	nun := make([]int, 0)
-	added := make(map[int]bool)
-	for _, val := range a {
-		if cnt[val] == 1 {
-			un = append(un, val)
-		} else if !added[val] {
-			nun = append(nun, val)
-		}
-		added[val] = true
-	}
-	return un, nun
+func run(a []int, n int) int {
+	sort.Slice(a, func(i, j int) bool {
+		return a[i] < a[j]
+	})
+	return a[len(a)-n]
 }
 
 func main() {
 	a := []int{1, 2, 6, 5, 4, 4}
-	fmt.Print(run(a))
+	fmt.Print(run(a, 1))
 }

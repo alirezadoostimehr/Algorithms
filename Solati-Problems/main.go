ff996dd772354c341897aaa92e186c5798cd3ab0
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-15 12:45:00 +0330

    new problem

--- Solati-Problems/main.go
+++ Solati-Problems/main.go
@@ -2,17 +2,27 @@
 // It also can be done in O(n * Log(n)) with using map
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func run(a []int) int {
-	result := 0
-	for _, val := range a {
-		result ^= val
+type pii struct {
+	first  int
+	secont int
+}
+
+func run(a []int, sum int) pii {
+	ind := make(map[int]int)
+	for i, val := range a {
+		if ind[sum-val] != 0 {
+			return pii{ind[sum-val] - 1, i}
+		}
+		ind[val] = i + 1
 	}
-	return result
+	return pii{-1, -1}
 }
 
 func main() {
-	a := []int{1, 2, 3, 1, 2}
-	fmt.Print(run(a))
+	a := []int{1, 2, 3, 5, 7, 11}
+	fmt.Print(run(a, 16))
 }

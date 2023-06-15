26a9e8e77be57dc62c16f5636bab2a5b7a789cd6
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-15 13:11:09 +0330

    new problem

--- Solati-Problems/main.go
+++ Solati-Problems/main.go
@@ -5,16 +5,27 @@ import (
 	"fmt"
 )
 
-func run(a []int) int {
-	n := len(a) + 1
-	sum := (n * (n + 1)) / 2
+func run(a []int) ([]int, []int) {
+	cnt := make(map[int]int)
+
 	for _, val := range a {
-		sum -= val
+		cnt[val]++
 	}
-	return sum
+	un := make([]int, 0)
+	nun := make([]int, 0)
+	added := make(map[int]bool)
+	for _, val := range a {
+		if cnt[val] == 1 {
+			un = append(un, val)
+		} else if !added[val] {
+			nun = append(nun, val)
+		}
+		added[val] = true
+	}
+	return un, nun
 }
 
 func main() {
-	a := []int{1, 2, 6, 5, 4}
+	a := []int{1, 2, 6, 5, 4, 4}
 	fmt.Print(run(a))
 }

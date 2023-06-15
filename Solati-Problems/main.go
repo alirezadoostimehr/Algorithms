bbcb926c250084a16b77b0cf6eaabde4130f86f8
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-15 13:23:00 +0330

    new problem

--- Solati-Problems/main.go
+++ Solati-Problems/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func run(a []int, n int) int {
@@ -10,13 +11,20 @@ func run(a []int, n int) int {
 	for _, val := range a {
 		cnt[val]++
 	}
+	keys := make([]int, 0)
+	for key := range cnt {
+		keys = append(keys, key)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
 	n = len(a) - n
 	now := 0
-	for key, val := range cnt {
-		if now+val >= n {
-			return key
+	for _, val := range keys {
+		now += cnt[val]
+		if now >= n {
+			return val
 		}
-		now += n
 
 	}
 	return 0
@@ -24,5 +32,5 @@ func run(a []int, n int) int {
 
 func main() {
 	a := []int{1, 2, 6, 5, 4, 4}
-	fmt.Print(run(a, 1))
+	fmt.Print(run(a, 3))
 }

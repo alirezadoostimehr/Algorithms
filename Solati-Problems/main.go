5393ed8e06665f7dcaf181f25cb6de9e46f5d4b2
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-16 15:07:23 +0330

    new problem

--- Solati-Problems/main.go
+++ Solati-Problems/main.go
@@ -3,23 +3,26 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func run(a []int) bool {
-	exists := make(map[int]bool)
+func run(a []int) int {
 	sum := 0
 	for _, val := range a {
-		if exists[val] {
-			return false
-		}
-		exists[val] = true
 		sum += val
 	}
-	n := len(a)
-	return sum == (n*(n+1))/2
+	res := (int)(math.Abs((float64)(sum)))
+	s1 := 0
+	s2 := sum
+	for _, val := range a {
+		s1 += val
+		s2 -= val
+		res = (int)(math.Min(math.Abs((float64)(s1-s2)), (float64)(res)))
+	}
+	return res
 }
 
 func main() {
-	a := []int{1, 4}
+	a := []int{3, 1, 2, 4, 3}
 	fmt.Print(run(a))
 }
